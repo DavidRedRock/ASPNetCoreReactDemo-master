c7241eb6eb4b09ff6aaadbac0d336bf12d061f0e
DavidRedRock <davidyoungy@yahoo.com>
2022-08-01 16:08:05 +1000

    Fixed another CI/CD bug and improved the GitHub Actions YAML file.

--- aspnetserver/Program.cs
+++ aspnetserver/Program.cs
@@ -11,7 +11,7 @@ builder.Services.AddCors(options =>
             builder
             .AllowAnyMethod()
             .AllowAnyHeader()
-            .WithOrigins("http://localhost:3000", "https://calm-water-04859b403.azurestaticapps.net");
+            .WithOrigins("http://localhost:3000", "https://proud-mushroom-0acaded00.1.azurestaticapps.net");
         });
 });
 
